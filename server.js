087e538f7bfabcf7abcf55725896654d63e384f4
Eddie Molina <Eddiestcc@gmail.com>
2024-07-31 13:24:13 -0400

    connecting server to frontend

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const { controlSignIn } = require('./controllers/controlSignIn');
@@ -8,7 +6,6 @@ const { controlRegister } = require('./controllers/controlRegister.js');
 const { controlProfile } = require('./controllers/controlProfile');
 const { controlImage , controlApiCall } = require('./controllers/controlImage');
 
-const port = process.env.PORT || 8080;
 
 // DATABASE
 // KNEX
@@ -19,7 +16,7 @@ const knex = require('knex')({
       port: 5432,
       user: 'mydb_b0sz_user',
       password: 'gpSLxC5UmrIcojhmpzsuCLH5dsjja77N',
-      database: 'smart-mydb_b0sz',
+      // database: 'smart-mydb_b0sz',
     },
   });
 
@@ -45,19 +42,6 @@ app.get('/profile/:id', (req,res) => controlProfile(req,res,knex));
 app.put('/image' , (req,res) => controlImage(req,res,knex));
 // API Call
 app.post('/imageurl' , (req,res) => controlApiCall(req,res));
-
-// // BUILD ENV
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/ping', function (req, res) {
-//  return res.send('pong');
-// });
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-app.listen(port);
 
 // CONSOLE 
 app.listen(3000, () => {
