cf9bc04660559ec08a576d06cae66147af233926
Eddie Molina <Eddiestcc@gmail.com>
2024-08-01 13:27:32 -0400

    fixing connection to database

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const knex = require('knex')({
     client: 'pg',
     connection: { 
       // connectionString: 'postgresql://mydb_b0sz_user:gpSLxC5UmrIcojhmpzsuCLH5dsjja77N@dpg-cqkhd0rv2p9s738jfsj0-a/mydb_b0sz',
-      host: 'ddpg-cqkhd0rv2p9s738jfsj0-a.oregon-postgres.render.com' ,
+      connectionString: 'postgresql://mydb_b0sz_user:gpSLxC5UmrIcojhmpzsuCLH5dsjja77N@dpg-cqkhd0rv2p9s738jfsj0-a.oregon-postgres.render.com/mydb_b0sz',
+      // connectionString: 'ddpg-cqkhd0rv2p9s738jfsj0-a.oregon-postgres.render.com' ,
+      hostname: 'dpg-cqkhd0rv2p9s738jfsj0-a',
       port: PORT,
       username: 'mydb_b0sz_user',
       password: 'gpSLxC5UmrIcojhmpzsuCLH5dsjja77N',
