b8dd9b55957f4675d343b29576cb4dfd02f495a7
Eddie Molina <Eddiestcc@gmail.com>
2024-08-01 13:13:13 -0400

    fixing header error

--- controllers/controlRegister.js
+++ controllers/controlRegister.js
@@ -2,8 +2,8 @@ const controlRegister = (req,res,knex,bcrypt) => {
     const {email,name,password} = req.body;
     if (!email || !name || !password) {
       return res.status(400).json('incorrect form submission');
-    } else {
-        const hash = bcrypt.hashSync(password);
+    } 
+    const hash = bcrypt.hashSync(password);
         knex.transaction(trx => {
             trx.insert({
                 hash: hash,
@@ -27,8 +27,7 @@ const controlRegister = (req,res,knex,bcrypt) => {
             .then(trx.commit)
             .catch(trx.rollback)
         })
-    // .catch(err => res.status(400).json('Unable to register'));
-    }
+    .catch(err => res.status(400).json('Unable to register'));
 }
 
 exports.controlRegister = controlRegister;
